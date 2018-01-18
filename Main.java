1257fdfb62fb5b1f072c3f276100629272ad9265
alexlopeztt <33875888+alexlopeztt@users.noreply.github.com>
2018-01-18 18:45:42 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
-
+//Alexander Lopez
+//Integration Project
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		// sysout Ctrl+space
 	}
-
+	//Objects are instances of a class...like a house
+	//Class is code...like a blueprint
 }
